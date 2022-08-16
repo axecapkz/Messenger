db0c3d8b66532c47e431ae625ccc3df725f0460d
Azamat <azamatk@me.com>
2022-08-16 11:58:06 +0600

    Implemented User authentication via FirebaseAuth

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -189,13 +189,17 @@ class RegisterViewController: UIViewController, UINavigationControllerDelegate {
         }
         
         // Firebase Log In
-        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: {[weak self] authResult, error in
+            guard let strongSelf = self else {
+                return
+            }
             guard let result = authResult, error == nil else {
                 print("Error creating user")
                 return
             }
             let user = result.user
             print("Created user \(user)")
+            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
         })
     }
     
